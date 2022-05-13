d1634ca855bb08c1c197beef9d9ecfe50d54e60b
FabioMaione <fabio00maione@gmail.com>
2022-05-13 10:19:30 +0200

    change

--- n32.java
+++ n32.java
@@ -9,9 +9,36 @@ public class n32 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         boolean crescente = false;
+        boolean decrescente = false;
+        boolean ok = false;
+        boolean ok2 = true;
+
         int x = s.nextInt();
-        
+        int tmp;
+        while(x != 0){
+            tmp = x;
+            x = s.nextInt();
+            if(ok && x != 0){
+                if(x == tmp)
+                    ok2 = false;
+                if(x > tmp)
+                    crescente = true;
+                if(crescente && x < tmp)
+                    decrescente = true;
+                if(crescente && decrescente)
+                    ok = true;
+                if(decrescente && x >= tmp){
+                    ok = false;
+                    ok2 = false;
+                }
+            }
+        }
+        if(ok && ok2)
+            System.out.println("SI");
+        else 
+            System.out.println("NO");
 
         s.close();
     }
 }
+//todo::correggere
